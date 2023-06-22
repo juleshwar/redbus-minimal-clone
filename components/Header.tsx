465614e3d748a9dbc09be35e7a8619776f635ffa
Joobie <dev.cleek@simplelogin.co>
2023-06-22 17:36:15 +0530

    feat: restructured code folders and added Header component

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Logo from "@/public/logo.svg"
+import Link from "next/link"
+
+const VerticalDivider = () => {
+    return (
+        <div className="mx-8 text-gray-400"> | </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div className="flex items-center px-8 py-3 sticky shadow drop-shadow-md shadow-neutral-200">
+            <Logo className="mx-6 w-[80px]" />
+            <VerticalDivider />
+            <div className="flex w-full">
+                <div className="flex items-center w-full">
+                    <Link href={'/'}>Bus Tickets</Link>
+                </div>
+                <div className="flex items-center col-auto">Profile</div>
+            </div>
+        </div>
+    )
+}
+
+export { Header }
