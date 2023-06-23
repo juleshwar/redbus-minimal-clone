74ea193d899f78dd97b94862312ba3ddb1161d26
Joobie <dev.cleek@simplelogin.co>
2023-06-23 17:11:32 +0530

    feat: adding new components
    TODO: styling

---
+++ components/InputWithAutosuggestion.tsx
@@ -0,0 +1,70 @@
+import { classNames } from "@/util/classNames";
+import { ChangeEvent, FocusEvent, useState } from "react";
+import { SuggestionDropdown, SuggestionItem } from "./SuggestionDropdown";
+
+interface Props {
+  label?: string;
+  value: string;
+  onChangeText: (newText: string) => void;
+  suggestions: Suggestion[];
+}
+
+export interface Suggestion {
+  id: number;
+  name: string;
+}
+
+const InputWithAutosuggestion = (props: Props) => {
+  const { label, value, onChangeText: onChangeText, suggestions } = props;
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const onClickSuggestionHandler = (item: SuggestionItem) => {
+    onChangeText(item.name);
+    setShowSuggestions(false);
+  };
+
+  const onChangeTextHandler = ({
+    currentTarget,
+  }: ChangeEvent<HTMLInputElement>) => {
+    onChangeText(currentTarget.value);
+    setShowSuggestions(true);
+  };
+
+  const onBlurHandler = (event: FocusEvent) => {
+    console.log(
+      "event.relatedTarget",
+      event.currentTarget,
+      event.relatedTarget
+    );
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      setShowSuggestions(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex relative" onBlur={onBlurHandler}>
+        <div className="flex">
+          <label className="">
+            <span>{label}</span>
+          </label>
+          <input type="text" value={value} onChange={onChangeTextHandler} />
+        </div>
+        <div
+          className={classNames([
+            "border border-blue-600",
+            "absolute top-full",
+            !showSuggestions && "hidden",
+          ])}
+        >
+          <SuggestionDropdown
+            items={suggestions}
+            onClickItem={onClickSuggestionHandler}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { InputWithAutosuggestion };
