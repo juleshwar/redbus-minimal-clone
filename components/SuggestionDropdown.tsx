aa0c08030039c71cdb8cf2f23a40964cdd4198a4
Joobie <dev.cleek@simplelogin.co>
2023-06-26 21:50:47 +0530

    feat: minor interface change

--- components/SuggestionDropdown.tsx
+++ components/SuggestionDropdown.tsx
@@ -1,29 +1,29 @@
 export interface SuggestionItem {
-  id: number;
-  name: string;
+	id: number | string;
+	name: string;
 }
 
 interface SuggestionProps {
-  items: SuggestionItem[];
-  onClickItem: (item: SuggestionItem) => void;
+	items: SuggestionItem[];
+	onClickItem: (item: SuggestionItem) => void;
 }
 
 export const SuggestionDropdown = ({ items, onClickItem }: SuggestionProps) => {
-  return (
-    <ul className="w-full flex flex-col">
-      {items.map((item) => {
-        const { id, name } = item;
-        return (
-          <li
-            className="cursor-default hover:bg-gray-200"
-            key={id}
-            onClick={() => onClickItem(item)}
-            onMouseDown={(event) => event.preventDefault()}
-          >
-            {name}
-          </li>
-        );
-      })}
-    </ul>
-  );
+	return (
+		<ul className="w-full flex flex-col">
+			{items.map((item) => {
+				const { id, name } = item;
+				return (
+					<li
+						className="cursor-default hover:bg-gray-200"
+						key={id}
+						onClick={() => onClickItem(item)}
+						onMouseDown={(event) => event.preventDefault()}
+					>
+						{name}
+					</li>
+				);
+			})}
+		</ul>
+	);
 };
