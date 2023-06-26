a8f07fbf6fc4337bfe8c8c4545fec23dfa0588fb
Joobie <dev.cleek@simplelogin.co>
2023-06-26 20:00:15 +0530

    refactor: modified travel location APIs

--- lib/travel-locations.ts
+++ lib/travel-locations.ts
@@ -5,38 +5,51 @@ export enum LOCATION_ID {
 	HYDERABAD = "004",
 	BANGALORE = "005",
 }
+export interface LocationDetails {
+	name: string;
+}
 
-export function getAllTravelLocations() {
-	return new Map([
-		[
-			LOCATION_ID.MADURAI,
-			{
-				name: "Madurai",
-			},
-		],
-		[
-			LOCATION_ID.COIMBATORE,
-			{
-				name: "Coimbatore",
-			},
-		],
-		[
-			LOCATION_ID.CHENNAI,
-			{
-				name: "Chennai",
-			},
-		],
-		[
-			LOCATION_ID.HYDERABAD,
-			{
-				name: "Hyderabad",
-			},
-		],
-		[
-			LOCATION_ID.BANGALORE,
-			{
-				name: "Bangalore",
-			},
-		],
-	]);
+const MADURAI_DETAILS: LocationDetails = {
+	name: "Madurai",
+};
+const COIMBATORE_DETAILS: LocationDetails = {
+	name: "Coimbatore",
+};
+const CHENNAI_DETAILS: LocationDetails = {
+	name: "Chennai",
+};
+const HYDERABAD_DETAILS: LocationDetails = {
+	name: "Hyderabad",
+};
+const BANGALORE_DETAILS: LocationDetails = {
+	name: "Bangalore",
+};
+
+const LOCATION_DETAILS = {
+	[LOCATION_ID.MADURAI]: MADURAI_DETAILS,
+	[LOCATION_ID.COIMBATORE]: COIMBATORE_DETAILS,
+	[LOCATION_ID.CHENNAI]: CHENNAI_DETAILS,
+	[LOCATION_ID.HYDERABAD]: HYDERABAD_DETAILS,
+	[LOCATION_ID.BANGALORE]: BANGALORE_DETAILS,
+};
+
+export function getAllLocationIds(): LOCATION_ID[] {
+	return [
+		LOCATION_ID.MADURAI,
+		LOCATION_ID.COIMBATORE,
+		LOCATION_ID.CHENNAI,
+		LOCATION_ID.HYDERABAD,
+		LOCATION_ID.BANGALORE,
+	];
+}
+
+export function getAllLocationIdToNameMap() {
+	return getAllLocationIds().reduce((acc, locId) => {
+		acc[locId] = LOCATION_DETAILS[locId].name;
+		return acc;
+	}, {} as Record<LOCATION_ID, string>);
+}
+
+export function getAllTravelLocationsWithDetails() {
+	return LOCATION_DETAILS;
 }
